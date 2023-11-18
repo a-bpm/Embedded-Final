314bd1f80ac834ee8ae955ec38e2e9e7a08590a9
Andrew <andrew.bryan.dev@gmail.com>
2023-11-18 11:40:58 -0500

    fix includes, clean up loop

--- Final.ino
+++ Final.ino
@@ -1,32 +1,31 @@
 #include <Servo.h>
-#include "ultrasonic.h"
-#include "motor.h"
+#include <IRremote.h>
+#include "include/Ultrasonic.hpp"
+#include "include/Motor.hpp"
 
-// Global constants
-
-// US pins
-const byte ULTRASONIC_ECHO = A0;
-const byte ULTRASONIC_TRIGGER = A1;
+/// Global constants
+// Ultrasonic pins
+const byte ULTRASONIC_TRIGGER_PIN = A1;
+const byte ULTRASONIC_ECHO_PIN = A0;
 
 // servo signal pin
 const byte SERVO_PIN = A2;
 
 // ir pin
-const byte INFRARED_REMOTE_PIN = 12;
+const byte IR_RECEIVER_PIN = 12;
 
 // right motor pins
-const byte MOTOR_RIGHT_ENABLE = 5;
-const byte MOTOR_RIGHT_FORWARD = 4;
-const byte MOTOR_RIGHT_BACKWARD = 2;
+const byte MOTOR_RIGHT_ENABLE_PIN = 5;
+const byte MOTOR_RIGHT_FORWARD_PIN = 4;
+const byte MOTOR_RIGHT_BACKWARD_PIN = 2;
 
 // left motor pins
-const byte MOTOR_LEFT_ENABLE = 6;
-const byte MOTOR_LEFT_FORWARD = 8;
-const byte MOTOR_LEFT_BACKWARD = 7;
+const byte MOTOR_LEFT_ENABLE_PIN = 6;
+const byte MOTOR_LEFT_FORWARD_PIN = 8;
+const byte MOTOR_LEFT_BACKWARD_PIN = 7;
 
 /// Global Variables
-
-// US
+// Ultrasonic
 Ultrasonic ultrasonicSensor(ULTRASONIC_TRIGGER_PIN, ULTRASONIC_ECHO_PIN);
 
 // Servo
@@ -42,8 +41,6 @@ Motor rightMotor;
 byte speed = 0;
 const int DELAY = 6 * 1000;
 
-// IR_S
-
 void setup() {
     // set up console
     Serial.begin(9600);
@@ -53,17 +50,17 @@ void setup() {
     // set up US
 
     // set up Motors
-    leftMotor = Motor(MOTOR_LEFT_FORWARD, MOTOR_LEFT_BACKWARD, MOTOR_LEFT_ENABLE);
-    rightMotor = Motor(MOTOR_RIGHT_FORWARD, MOTOR_RIGHT_BACKWARD, MOTOR_RIGHT_ENABLE);
+    leftMotor = Motor(MOTOR_LEFT_FORWARD_PIN, MOTOR_LEFT_BACKWARD_PIN, MOTOR_LEFT_ENABLE_PIN);
+    rightMotor = Motor(MOTOR_RIGHT_FORWARD_PIN, MOTOR_RIGHT_BACKWARD_PIN, MOTOR_RIGHT_ENABLE_PIN);
 
     // set up IR
-    IrReceiver.begin(IR_RECEIVE_PIN, true);
+    IrReceiver.begin(IR_RECEIVER_PIN, true);
 
     // ?
-    pinMode(SIG_PIN, OUTPUT);
-    pinMode(TRIG_PIN, OUTPUT);
-    digitalWrite(SIG_PIN, LOW);
-    digitalWrite(TRIG_PIN, LOW);
+    pinMode(ULTRASONIC_ECHO_PIN , OUTPUT);
+    pinMode(ULTRASONIC_TRIGGER_PIN, OUTPUT);
+    digitalWrite(ULTRASONIC_ECHO_PIN, LOW);
+    digitalWrite(ULTRASONIC_TRIGGER_PIN, LOW);
 
   // set up timer1 count
     TCNT1H = 0xF3;
@@ -82,49 +79,50 @@ void setup() {
     interrupts();
 }
 
+bool ultrasonicInterrupt = false;
 void loop() {
 
-  if(flag) {
-
+  if(ultrasonicInterrupt) {
 
   }
   /// look around (I have a feeling we can somehow make this process into a for loop or robot class method called three times)
   // for(int i = 0; i > 2; i++ ) // we can make the poistion change in this too i think but we can make explicit changes as well
   // look left
-    position = /*left*/;
+    //position = *left*/;
     // turn neck left
     neckServo.write(position);
   
     // measurement
-    unsigned long durationLeftLength = ultrasonicSensor.measure;
+    unsigned long durationLeftLength = ultrasonicSensor.measureInch();
     
     // delay
     delay(1300);
 
     // get ready to turn left
-    position = /*middle */
+    //position = /*middle */
   
   // look middle
     neckServo.write(position);
   
     // measurement
-    unsigned long durationMiddleLength = ultrasonicSensor.measure;
+    unsigned long durationMiddleLength = ultrasonicSensor.measureInch();
     
     // delay
     delay(1300);
 
     // get ready to turn left
-    position = /*right*/
+    //position = /*right*/
   // look right
     neckServo.write(position);
   
     // measurement
-    unsigned long durationRightLength = ultrasonicSensor.measure;
+    unsigned long durationRightLength = ultrasonicSensor.measureInch();
     
     // delay
     delay(1300);
 
   ///determine measurement
+  /*
   if(left > 14 && Forward > 14 && Right > 14)
   {
     Finished
@@ -154,14 +152,15 @@ void loop() {
   {
     Right Turn
   }
+  */
 
   // move
 
   // turn based on measure (we can use enums for the movement states)
-  switch(state)
+  //switch(state)
   
   // move forward
-  Serial.println("Forwwad..");
+  Serial.println("Forward...");
   leftMotor.run(Motor::MotorForward);
   rightMotor.run(Motor::MotorForward);
   delay(DELAY);
@@ -170,5 +169,5 @@ void loop() {
 
 
 ISR(TIMER_OCF_vect) {
-    interrupted = true;
+    ultrasonicInterrupt = true;
 }
